a77ec463d65177d7f545b6ce16b1d4a1fdecad67
Demonsr8 <95967173+Demonsr8@users.noreply.github.com>
2021-12-11 15:00:29 +0200

    Delete php directory

--- php/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $db = mysqli_connect ('sql206.ruhost.ga','rooru_28394074','QYJ748qGNvYqPs2','rooru_28394074_users');
-	mysqli_set_charset($db, "utf8");
-?>
--- php/exit.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header('HTTP/1.1 200 OK');
-header('Location: http://'.$_SERVER['HTTP_HOST'].'/index.html');
-exit();
-?>
